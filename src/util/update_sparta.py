5874f5d25d490c40898e3b67a4aa664e5f478fa6
Adrian Medina <adrian@wikipunk.net>
2024-05-22 18:18:57 -0400

    Add SPARTA countermeasures with semantic links

--- src/util/update_sparta.py
+++ src/util/update_sparta.py
@@ -42,6 +42,7 @@ def get_sparta_id(tech):
             ref.get("external_id")
             for ref in tech["external_references"]
             if ref.get("source_name") == "sparta"
+            and not ref.get("external_id").startswith("D3")
         ),
         None,
     )
@@ -58,7 +59,7 @@ def add_technique_to_graph(src, g, tech):
     # If the technique has a SPARTA ID, add it to the graph
     if sparta_id is not None:
         # Create a URI for the SPARTA Technique
-        sparta_uri = D3F[f"SPARTA-{sparta_id}"]
+        sparta_uri = D3F[f"{sparta_id}"]
         g.add((sparta_uri, RDF.type, D3F.SPARTATechnique))
         g.add((sparta_uri, RDF.type, OWL.Class))
         g.add((sparta_uri, RDF.type, OWL.NamedIndividual))
@@ -81,7 +82,7 @@ def add_technique_to_graph(src, g, tech):
                 (
                     sparta_uri,
                     RDFS.subClassOf,
-                    D3F[f"SPARTA-{sparta_id.split('.')[0]}"],
+                    D3F[f"{sparta_id.split('.')[0]}"],
                 )
             )
         else:
@@ -102,7 +103,7 @@ def add_threat_to_graph(src, g, threat):
     # If the threat has a SPARTA ID, add it to the graph
     if sparta_id is not None:
         # Create a URI for the SPARTA Threat
-        sparta_uri = D3F[f"SPARTA-{sparta_id}"]
+        sparta_uri = D3F[f"{sparta_id}"]
         g.add((sparta_uri, RDF.type, D3F.SPARTAThreat))
         g.add((sparta_uri, RDF.type, OWL.Class))
         g.add((sparta_uri, RDF.type, OWL.NamedIndividual))
@@ -137,7 +138,7 @@ def add_threat_to_graph(src, g, threat):
         # Add relationships to related SPARTA Techniques
         for rel in src.relationships(threat):
             related_uri = D3F[
-                f"SPARTA-{src.get(rel['target_ref'])['external_references'][0]['external_id']}"
+                f"{src.get(rel['target_ref'])['external_references'][0]['external_id']}"
             ]
             g.add((sparta_uri, D3F.related, related_uri))
 
@@ -149,7 +150,92 @@ def add_threat_to_graph(src, g, threat):
         #         g.add((sparta_uri, D3F.related, capec_uri))
 
 
-def get_sparta_graph(sparta_path):
+def add_countermeasure_to_graph(src, g, d3fend_graph, countermeasure):
+    """
+    Add a SPARTA Countermeasure to the graph
+    :param src: MemoryStore
+    :param g: Graph
+    :param d3fend_graph: Graph of D3FEND Ontology
+    :param countermeasure: STIX course-of-action object that is a SPARTA Countermeasure
+    """
+    sparta_id = get_sparta_id(countermeasure)
+    # If the countermeasure has a SPARTA ID, add it to the graph
+    if sparta_id is not None:
+        # Create a URI for the SPARTA Countermeasure
+        sparta_uri = D3F[f"{sparta_id}"]
+        g.add((sparta_uri, RDF.type, D3F.SPARTACountermeasure))
+        g.add((sparta_uri, RDF.type, OWL.Class))
+        g.add((sparta_uri, RDF.type, OWL.NamedIndividual))
+        g.add((sparta_uri, RDFS.label, Literal(countermeasure["name"])))
+        sparta_url = next(
+            (
+                ref.get("url")
+                for ref in countermeasure["external_references"]
+                if ref.get("source_name") == "sparta"
+            ),
+            None,
+        )
+        g.add((sparta_uri, RDFS.seeAlso, URIRef(sparta_url)))
+        g.add((sparta_uri, D3F.definition, Literal(countermeasure["description"])))
+        g.add((sparta_uri, D3F["sparta-id"], Literal(sparta_id)))
+
+        # Add relationships to related SPARTA classes
+        for rel in src.relationships(countermeasure):
+            d3fend_id = next(
+                (
+                    ref["external_id"]
+                    for ref in src.get(rel["target_ref"])["external_references"]
+                    if ref["source_name"] == "D3FEND"
+                ),
+                None,
+            )
+            # if the related object is a D3FEND technique then it is a countermeasure for that technique
+            if d3fend_id is not None:
+                d3fend_name = (
+                    next(
+                        (
+                            ref["url"]
+                            for ref in src.get(rel["target_ref"])["external_references"]
+                            if ref["source_name"] == "D3FEND"
+                        ),
+                        None,
+                    )
+                    .replace("https://d3fend.mitre.org/technique/d3f:", "")
+                    .replace("/", "")
+                )
+                if d3fend_name is not None:
+                    d3fend_technique = D3F[d3fend_name]
+                    g.add((sparta_uri, D3F["enabled-by"], d3fend_technique))
+            # else if the related object is a SPARTA technique then it is a countermeasure for that technique
+            elif (
+                src.get(rel["target_ref"])["type"] == "attack-pattern"
+                and sparta_id != "CM0000"
+            ):
+                related_uri = D3F[
+                    f"{src.get(rel['target_ref'])['external_references'][0]['external_id']}"
+                ]
+                g.add((sparta_uri, D3F.counters, related_uri))
+
+        # for NIST control mappings referenced as external references in the countermeasure
+        for ref in countermeasure.get("external_references", []):
+            if ref.get("url").startswith(
+                "https://sparta.aerospace.org/countermeasures/references/"
+            ):
+                nist_control_id = (
+                    ref.get("external_id").replace("(", "_").replace(")", "")
+                )
+                nist_control = D3F["NIST_SP_800-53_R5_" + nist_control_id]
+                # g.add((sparta_uri, D3F.related, URIRef(ref.get('url'))))
+                g.add((sparta_uri, D3F.related, nist_control))
+
+
+def get_sparta_graph(sparta_path, d3fend_graph):
+    """
+    Get a graph of SPARTA Techniques and Threats
+    :param sparta_path: Path to SPARTA JSON data
+    :param d3fend_graph: Graph of D3FEND Ontology
+    :return: Graph of SPARTA Techniques and Threats
+    """
     src = MemoryStore()
     src.load_from_file(sparta_path)
 
@@ -182,6 +268,13 @@ def get_sparta_graph(sparta_path):
         if threat.get("kill_chain_phases") is None
     ]
 
+    countermeasures = src.query(
+        [
+            Filter("type", "=", "course-of-action"),
+            Filter("external_references.source_name", "=", "sparta"),
+        ]
+    )
+
     # Create a new graph
     g = Graph()
 
@@ -193,6 +286,10 @@ def get_sparta_graph(sparta_path):
     for threat in threats:
         add_threat_to_graph(src, g, threat)
 
+    # Add SPARTA Countermeasures to the graph
+    for countermeasure in countermeasures:
+        add_countermeasure_to_graph(src, g, d3fend_graph, countermeasure)
+
     return g
 
 
@@ -201,7 +298,9 @@ def main(SPARTA_VERSION="1.6"):
     d3fend_graph = Graph()
     d3fend_graph.parse("src/ontology/d3fend-protege.sparta.ttl")
 
-    sparta_graph = get_sparta_graph(f"data/sparta_data_v{SPARTA_VERSION}.json")
+    sparta_graph = get_sparta_graph(
+        f"data/sparta_data_v{SPARTA_VERSION}.json", d3fend_graph
+    )
 
     d3fend_graph += sparta_graph
 
